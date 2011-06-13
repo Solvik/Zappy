975304effdfb7c12fb9f97a786ddf10ea3aefbdb
Julien Di Marco <juliendimarco@me.com>
2011-06-13 18:47:04 +0200

    added a new option as a kind of bonus

--- server_zappy/include/zopt.h
+++ server_zappy/include/zopt.h
@@ -5,8 +5,17 @@
 typedef		struct
 {
   int		port;
+  int		width;
+  int		height;
   int		nb_client;
   int		delay;
+  int		time;
 }		t_zopt;
+
+/*
+** time should be a great option to change the time referential
+** wich by default is 1 second aka 1 000 000 usec.
+**
+*/
 
 #endif		/* !ZAPPY_OPT */
