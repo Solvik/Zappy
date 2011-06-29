9b7cb516ed7f6a4d49682b263b78cafcc34abedb
guillaume gelin <gelin_g@cd.(none)>
2011-06-29 15:10:12 +0200

    protocole graphique : commande tna

--- server_zappy/modules/graph_protocol/src/graph_team.c
+++ server_zappy/modules/graph_protocol/src/graph_team.c
@@ -5,16 +5,27 @@
 ** Login   <seb@epitech.net>
 **
 ** Started on  Mon Jun 13 19:01:45 2011 seb
-** Last update Mon Jun 13 19:01:45 2011 seb
+** Last update Wed Jun 29 15:08:52 2011 guillaume gelin
 */
 
+#include "list.h"
+#include "team.h"
 #include "network.h"
 #include "graph_team.h"
 
 bool graph_tna(t_fds *player, char *cmd)
 {
-  sends(player, "tna LOL");
-  /*
-    On envoie tna tean_name\n pour chaque team presente sur la map
-   */
+  unsigned int	i;
+  t_list	*teams;
+  char		*to_send;
+  void		*data;
+
+  i = 0;
+  teams = get_teams();
+  while ((data = get_data_at(teams, i)) && ++i)
+    {
+      asprintf(&to_send, "tna %s", ((t_team *)data)->name);
+      sends(player, to_send);
+      free(to_send);
+    }
 }
