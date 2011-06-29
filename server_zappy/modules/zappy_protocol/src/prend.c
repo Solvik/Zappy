8c3bb7f68bd310495bfd97343ca88f1bb83f0759
Solvik <solvik.blum@gmail.com>
2011-06-29 17:49:08 +0200

    prend function done

--- server_zappy/modules/zappy_protocol/src/prend.c
+++ server_zappy/modules/zappy_protocol/src/prend.c
@@ -5,19 +5,68 @@
 ** Login   <blum_s@epitech.net>
 **
 ** Started on  Mon Jun 13 12:46:13 2011 solvik blum
-** Last update Wed Jun 29 17:12:36 2011 solvik blum
+** Last update Wed Jun 29 17:47:37 2011 solvik blum
 */
 
 #include <stdlib.h>
 
 #include "player.h"
+#include "map.h"
+#include "voir.h"
 #include "server_zappy.h"
 
+e_stone		is_stone(char *str)
+{
+  int		i;
+
+  i = 0;
+  while (i < size_stone)
+    {
+      if (!strcmp(str, &(gl_ressource_name[i].str[1])))
+	return (gl_ressource_name[i].stone);
+      i++;
+    }
+  return (NONE);
+}
+
+static bool	prendre_action(int x, int y, char *obj)
+{
+  t_box		*map;
+  e_stone	stone;
+
+  map = get_map();
+  if ((stone = is_stone(obj)) != NONE)
+    {
+      if (setbox_delstone(x, y, stone, 1) == true)
+	return (true);
+      else
+	return (false);
+    }
+  else if (!strcmp(obj, "nourriture"))
+    {
+      /* if (setbox_delfood(x, y, 1) == true) */
+      /* 	return (true); */
+      /* else */
+      /* 	return (false); */
+    }
+  else
+    return (false);
+}
 
 int		zappy_prend(t_fds *client, char *cmd)
 {
-  (void)cmd;
-  sends(client, "ok");
+  char		*obj;
 
-  return (1);
+  strtok(cmd, " \t");
+  obj = strtok(NULL, " \t");
+  if (obj)
+    {
+      if (prendre_action(player_data->x, player_data->y, obj))
+	{
+	  sends(client, "ok");
+	  return (1);
+	}
+    }
+  sends(client, "ko");
+  return (0);
 }
