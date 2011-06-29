b1c86e874c704db019fb19489e56b4f77e51a808
db0 <zero.fansub@gmail.com>
2011-06-29 13:22:04 +0200

    Include

--- server_zappy/include/tserver.h
+++ server_zappy/include/tserver.h
@@ -28,18 +28,20 @@ t_box *		get_map(void);
 uint		get_map_max(void);
 t_box *		get_box(uint, uint);
 t_list *	get_modules(void);
-
+uint		getbox_nbstones(uint, uint, e_stone);
 int		get_port(void);
-
 fds *		get_pool(void);
+t_list *	get_players(unit, uint);
 
 void		set_map_width(uint);
 void		set_map_height(uint);
 void		set_map(t_box *);
 bool		set_new_module(t_module *);
-
+bool		setbox_addstone(uint, uint, e_stone, uint);
+bool		setbox_delstone(uint, uint, e_stone, uint);
 void		set_port(int);
+bool		add_player(t_player *, char *, uint, uint);
 
-bool		gserv_const(bool created);
+bool		gserv_const(bool);
 
 #endif		/* !TSERV_H_ */
