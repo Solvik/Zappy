d4c62cba6e30e2c14b310f37ed15a6401e82bc11
Solvik <solvik.blum@gmail.com>
2011-06-30 09:24:17 +0200

    the zappy protocol can work now

--- server_zappy/modules/zappy_protocol/src/connect_nbr.c
+++ server_zappy/modules/zappy_protocol/src/connect_nbr.c
@@ -5,10 +5,17 @@
 ** Login   <blum_s@epitech.net>
 **
 ** Started on  Mon Jun 13 12:46:13 2011 solvik blum
-** Last update Tue Jun 14 18:21:13 2011 solvik blum
+** Last update Thu Jun 30 09:24:09 2011 solvik blum
 */
 
 #include <stdlib.h>
 
 #include "player.h"
 #include "server_zappy.h"
+
+
+int		zappy_connect_nbr(t_fds *client, char *cmd)
+{
+  sends(client, "ok");
+  return (1);
+}
