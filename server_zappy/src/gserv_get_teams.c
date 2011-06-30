222f46bae8aed77e210e5d54e58e51a13a8818de
Sebastien Blot <seb@seb-desktop.(none)>
2011-06-30 09:20:44 +0200

    compilation fix

--- server_zappy/src/gserv_get_teams.c
+++ server_zappy/src/gserv_get_teams.c
@@ -1,13 +1,14 @@
 /*
 ** gserv_get_teams.c for  in /Users/Lifely/Developer/project/Zappy/server_zappy
-** 
+**
 ** Made by Julien Di Marco
 ** Login   <Lifely@epitech.net>
-** 
+**
 ** Started on  Thu Jun 30 05:05:18 2011 Julien Di Marco
 ** Last update Thu Jun 30 05:05:18 2011 Julien Di Marco
 */
 
+#include	<stdlib.h>
 #include	"tserver.h"
 
 extern t_server *gserv;
