a6063ff75922d4b3f6c66dcb220c2731cce81236
Solvik <solvik.blum@gmail.com>
2011-07-03 23:03:19 +0200

    Incantation is done

--- server_zappy/modules/zappy_protocol/src/incantation.c
+++ server_zappy/modules/zappy_protocol/src/incantation.c
@@ -5,18 +5,74 @@
 ** Login   <blum_s@epitech.net>
 **
 ** Started on  Mon Jun 13 12:46:13 2011 solvik blum
-** Last update Wed Jun 29 16:31:01 2011 solvik blum
+** Last update Sun Jul  3 23:02:55 2011 solvik blum
 */
 
-#include <stdlib.h>
+#define		_GNU_SOURCE
 
-#include "player.h"
-#include "server_zappy.h"
+#include	<stdio.h>
+#include	<stdlib.h>
+
+#include	"player.h"
+#include	"stone.h"
+#include	"server_zappy.h"
+#include	"tserver.h"
+
+typedef struct	s_elevation
+{
+  uint		nb_players;
+  uint		linemate;
+  uint		deraumere;
+  uint		sibur;
+  uint		mendiane;
+  uint		phiras;
+  uint		thystame;
+}		t_elevation;
+
+static const t_elevation      gl_elevation_infos[] =
+  {
+    {1, 1, 0, 0, 0, 0, 0},
+    {2, 1, 1, 1, 0, 0, 0},
+    {2, 2, 0, 1, 0, 2, 0},
+    {4, 1, 1, 2, 0, 1, 0},
+    {4, 1, 2, 1, 3, 0, 0},
+    {6, 1, 2, 3, 0, 1, 0},
+    {6, 2, 2, 2, 2, 2, 1}
+  };
+
+#define ELV	gl_elevation_infos[player->level]
+
+static bool	verif_incantation(t_player *player)
+{
+  if (ELV.linemate == getbox_nbstones_by_player(player, LINEMATE) &&
+      ELV.deraumere == getbox_nbstones_by_player(player, DERAUMERE) &&
+      ELV.sibur == getbox_nbstones_by_player(player, SIBUR) &&
+      ELV.mendiane == getbox_nbstones_by_player(player, MENDIANE) &&
+      ELV.phiras == getbox_nbstones_by_player(player, PHIRAS) &&
+      ELV.thystame == getbox_nbstones_by_player(player, THYSTAME) &&
+      ELV.nb_players == (uint)get_list_len(get_players_by_level(player->x,
+								player->y,
+								player->level)))
+    return (true);
+  return (false);
+}
 
 int		zappy_incantation(t_fds *client, char *cmd)
 {
-  (void)cmd;
+  int		r;
+  char		*msg;
 
-  sends(client, "ok");
+  (void)cmd;
+  if (!verif_incantation(player_data))
+    {
+      sends(client, "ko");
+      return (0);
+    }
+  sends(client, "elevation en cours");
+  player_data->level += 1;
+  r = asprintf(&msg, "niveau actuel : %d", player_data->level);
+  sends(client, msg);
+  if (msg)
+    free(msg);
   return (1);
 }
