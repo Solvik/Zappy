610ff2cef7a77146c0472e9be1651d7df58b5a98
Solvik <solvik.blum@gmail.com>
2011-07-03 23:22:23 +0200

    begin of fork

--- server_zappy/modules/zappy_protocol/src/fork.c
+++ server_zappy/modules/zappy_protocol/src/fork.c
@@ -5,16 +5,24 @@
 ** Login   <blum_s@epitech.net>
 **
 ** Started on  Mon Jun 13 12:46:13 2011 solvik blum
-** Last update Sun Jul  3 16:02:27 2011 solvik blum
+** Last update Sun Jul  3 23:22:06 2011 solvik blum
 */
 
-#include <stdlib.h>
+#include	<stdlib.h>
 
-#include "player.h"
-#include "server_zappy.h"
+#include	"player.h"
+#include	"egg.h"
+#include	"server_zappy.h"
 
 
 int		zappy_fork(t_fds *client, char *cmd)
 {
+  t_egg		*egg;
+
+  if (!(egg = setbox_add_egg(player_data)))
+    return (0);
+  /* scheduler_relative(client, 42, */
+  /* 		     (bool (*)(fds, void*))test, strdup(cmd))); */
+  sends(client, "ok");
   return (1);
 }
