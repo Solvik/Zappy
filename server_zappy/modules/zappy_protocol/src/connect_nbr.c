58cf8f5fb3c7bdf4620e2574648ecfaff84c2d62
Solvik <solvik.blum@gmail.com>
2011-07-03 22:36:39 +0200

    connect nbr fixes

--- server_zappy/modules/zappy_protocol/src/connect_nbr.c
+++ server_zappy/modules/zappy_protocol/src/connect_nbr.c
@@ -5,13 +5,17 @@
 ** Login   <blum_s@epitech.net>
 **
 ** Started on  Mon Jun 13 12:46:13 2011 solvik blum
-** Last update Sun Jul  3 22:32:39 2011 solvik blum
+** Last update Sun Jul  3 22:36:21 2011 solvik blum
 */
 
-#include <stdlib.h>
+#define		_GNU_SOURCE
+#include	<stdlib.h>
+#include	<stdio.h>
 
-#include "player.h"
-#include "server_zappy.h"
+#include	"player.h"
+#include	"team.h"
+#include	"server_zappy.h"
+#include	"tserver.h"
 
 
 int		zappy_connect_nbr(t_fds *client, char *cmd)
@@ -21,7 +25,7 @@ int		zappy_connect_nbr(t_fds *client, char *cmd)
   t_team	*team;
 
   (void)cmd;
-  team = get_team_of_player(player);
+  team = get_team_of_player(player_data);
   r = asprintf(&msg, "%d", team->max_conn);
   sends(client, msg);
   if (msg)
