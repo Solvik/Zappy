760df70b3994640101a70064fd78691798883c9d
Sebastien Blot <seb@seb-desktop.(none)>
2011-07-04 04:29:45 +0200

    broadcast almost finished, just need to handle orientation

--- server_zappy/modules/zappy_protocol/src/broadcast.c
+++ server_zappy/modules/zappy_protocol/src/broadcast.c
@@ -27,16 +27,66 @@ static void	send_broadcast(void *player)
   float		adj;
   float		angle;
 
-  new_x = ((t_player *)(player))->x + tr_x;
-  new_y = ((t_player *)(player))->y + tr_y;
+  new_x = (((t_player *)(player))->x + tr_x - get_map_width() / 2) % get_map_width();
+  new_y = (((t_player *)(player))->y + tr_y - get_map_height() / 2) % get_map_height();
   printf("player_x = %d player_y = %d new_x = %d new_y = %d\n", ((t_player *)(player))->x,((t_player *)(player))->y, new_x, new_y);
   printf("translation x %d translation y %d\n", tr_x, tr_y);
   hyp = sqrt(pow(-new_x ,2) + pow(-new_y, 2));
   adj = abs(new_x);
   angle = acos(adj / hyp);
   angle = (angle * 180) / 3.1415;
-  angle = 90 - angle;
+  angle = 90 - (int)angle;
   printf("angle = %f\n", angle);
+  if (0 == new_x || 0 == new_y)
+    {
+      if (0 == new_y && new_x < 0)
+	printf("p1\n");
+      if (0 == new_y && new_x > 0)
+	printf("p5\n");
+      if (0 == new_x && new_y > 0)
+	printf("p3\n");
+      if (0 == new_x && new_y < 0)
+	printf("p7\n");
+    }
+  else
+    {
+      if (new_x <= 0 && new_y >= 0)
+	{
+	  if (angle <= -25)
+	    printf("3\n");
+	  else if (angle > -25 && angle <= -65)
+	    printf("2\n");
+	  else
+	    printf("1\n");
+	}
+      else if (new_x <= 0 && new_y <= 0)
+	{
+	  if (angle >= -25)
+	    printf("1\n");
+	  else if (angle < -25 && angle >= -65)
+	    printf("8\n");
+	  else
+	    printf("7\n");
+	}
+      else if (new_x >= 0 && new_y >= 0)
+	{
+	  if (angle <= 25)
+	    printf("3\n");
+	  else if (angle > 25 && angle <= 65)
+	    printf("4\n");
+	  else
+	    printf("5\n");
+	}
+      else
+	{
+	  if (angle <= 25)
+	    printf("7\n");
+	  else if (angle > 25 && angle <= 65)
+	    printf("6\n");
+	  else
+	    printf("5\n");
+	}
+    }
 }
 
 int		zappy_broadcast(t_fds *client, char *cmd)
