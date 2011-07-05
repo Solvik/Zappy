a9c4e0ca56e7abd3a2e30c5c8357b72015ea3ce6
Solvik <solvik.blum@gmail.com>
2011-07-05 19:21:14 +0200

    client start.. time to sleep for a night of work

--- client_zappy/include/client_zappy.h
+++ client_zappy/include/client_zappy.h
@@ -1,18 +1,52 @@
 /*
 ** client_zappy.h for  in /Users/Lifely/Developer/project/Zappy/client_zappy
-** 
+**
 ** Made by Julien Di Marco
 ** Login   <Lifely@epitech.net>
-** 
+**
 ** Started on  Thu Jun 30 05:33:40 2011 Julien Di Marco
-** Last update Thu Jun 30 05:33:40 2011 Julien Di Marco
+** Last update Tue Jul  5 19:07:34 2011 solvik blum
 */
 
 #ifndef		CLIENT_ZAPPY_H_
 # define	CLIENT_ZAPPY_H_
 
 #  include	<stdbool.h>
+#  include	"ztypes.h"
 
 bool		client_zappy(void);
+
+typedef struct	s_sprite
+{
+  char		*grass;
+}		t_sprite;
+
+typedef struct	s_ressource
+{
+  char		*name;
+  uint		nb;
+}		t_ressource;
+
+typedef struct	s_box
+{
+  int		linemate;
+  int		deraumere;
+  int		sibur;
+  int		mendiane;
+  int		phiras;
+  int		thystame;
+  int		egg;
+  int		food;
+}		t_box;
+
+typedef struct	s_visualiseur
+{
+  SDL_Surface	*screen;
+  t_sprite	*sprite;
+  t_list	*map; // t_box
+  t_list	*player; // t_player
+}		t_visualiseur;
+
+typedef t_visualiseur t_visu;
 
 #endif		/* !CLIENT_ZAPPY_H_ */
--- client_zappy/src/client_zappy.c
+++ client_zappy/src/client_zappy.c
@@ -1,16 +1,48 @@
 /*
 ** client_zappy.c for  in /Users/Lifely/Developer/project/Zappy/client_zappy
-** 
+**
 ** Made by Julien Di Marco
 ** Login   <Lifely@epitech.net>
-** 
+**
 ** Started on  Thu Jun 30 05:33:31 2011 Julien Di Marco
-** Last update Thu Jun 30 05:33:31 2011 Julien Di Marco
+** Last update Tue Jul  5 19:21:04 2011 solvik blum
 */
 
 #include	"client_zappy.h"
 
-bool		client_zappy(void)
+void		create_window(t_visu *p)
 {
+  if (SDL_Init(SDL_INIT_VIDEO) == -1)
+    {
+      fprintf(stderr, "Can't init SDL. We're going to die\n");
+      exit(EXIT_FAILURE);
+    }
+  SDL_EnableKeyRepeat(1, 1);
+  SDL_WM_SetCaption("Zappy", NULL);
+  p->sprite->grass = "images/monster_left.png";
+}
+
+void		connect_server(char *addr, int port)
+{
+  sends(truc, "GRAPHIC");
+  // while on recoit, on traite selon cmd[0]
+}
+
+bool		client_zappy(char *addr, int port)
+{
+  t_visu	visu;
+
+  create_window(&visu);
+  connect_server(addr, port);
+  // event (souris)
+  // send > GRAPHIC
+  // on recupere les infos
+  // msz X Y > taille map
+  // sgt T > temps
+  // "bct 0 0 q q q q q q q\n" >> contenu de chaque case
+  // tna N >> nom de chaque equipe
+  // "pnw #n X Y O L N\n" >> connexion d'un nouveau joueur
+  // oeuf pondu >> ok
+
   return (true);
 }
