7ffd073a70a0eee506766dacf3509845e9e14725
Julien Di Marco <juliendimarco@me.com>
2011-07-07 19:17:10 +0200

    Little Error

--- server_zappy/src/map_random_inventory.c
+++ server_zappy/src/map_random_inventory.c
@@ -21,7 +21,7 @@ static e_stone	random_stone(void)
   static ssize_t	i = -1;
 
   return (stonetab[(i = (i < (sizeof(stonetab) / sizeof(*stonetab) ?
-			      i + 1 : 0)))]);
+			      i + 1 : -1)))]);
 }
 
 void		map_random_fill_stones(void)
@@ -29,7 +29,6 @@ void		map_random_fill_stones(void)
    size_t	i;
 
   i = 0;
-  printf("%f\n", (((double)map_stones_density / 100.0) * (double)get_map_max()));
   while (i < (((double)map_stones_density / 100.0) * (double)get_map_max()))
     {
       set_box_addstone(rand() % get_map_width(),
@@ -45,8 +44,6 @@ void		map_random_fill_food(void)
   size_t	i;
 
   i = 0;
-  printf("%d * %d  = %f \n", map_food_density, get_map_max(),
-	 (((double)map_food_density / 100.0) * (double)get_map_max()));
   while (i < (((double)map_food_density / 100.0) * (double)get_map_max()))
     {
       set_box_addfood(rand() % get_map_width(),
