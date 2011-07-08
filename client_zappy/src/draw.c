4aac806ba29d044b9f2cbf6a9acc178fad185088
Solvik <solvik.blum@gmail.com>
2011-07-09 01:27:38 +0200

    draw text base

--- client_zappy/src/draw.c
+++ client_zappy/src/draw.c
@@ -9,12 +9,26 @@
 */
 
 #include "client_zappy.h"
+#include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
 
 void		refresh_screen(t_visu *v)
 {
   draw_map(v);
   foreach_arg_list(v->player, draw_player, v);
   draw_info(v);
+}
+
+void		draw_text(TTF_Font *police, SDL_Surface *base, char *text, int coor[2])
+{
+  SDL_Surface *texte = NULL;
+  SDL_Color couleurNoire = {0, 0, 0, 0};
+  SDL_Rect position;
+
+  position.x = coor[0];
+  position.y = coor[1];  
+  texte = TTF_RenderText_Blended(police, text, couleurNoire);
+  SDL_BlitSurface(texte, NULL, base, &position);
 }
 
 void		draw_info(t_visu *v)
@@ -26,6 +40,24 @@ void		draw_info(t_visu *v)
   pos.x = 0;
   pos.y = 600 - 32;
   SDL_FillRect(v->info, NULL, SDL_MapRGB(v->screen->format, 0, 255, 0));
+
+  TTF_Font *police = NULL;
+  TTF_Init();
+  police = TTF_OpenFont("/usr/share/cups/fonts/FreeMono.ttf", 12);
+  int toto[2];
+  toto[1] = 0;
+  toto[0] = 0;
+  draw_text(police, v->info, "Linemate", toto);
+  toto[0] = 100;
+  draw_text(police, v->info, "Deraumere", toto);
+  toto[0] = 200;
+  draw_text(police, v->info, "Mendiane", toto);
+  toto[0] = 300;
+  draw_text(police, v->info, "Phiras", toto);
+  toto[0] = 400;
+  draw_text(police, v->info, "Thystame", toto);
+  toto[0] = 500;
+  draw_text(police, v->info, "Food", toto);
 }
 
 void		draw_map(t_visu *v)
