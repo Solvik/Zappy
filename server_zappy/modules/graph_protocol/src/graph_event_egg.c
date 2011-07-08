2d07a6c51b4b0a5e7f61a994e61d7ca92bce5c97
guillaume gelin <perso@ramnes.eu>
2011-07-08 02:41:44 +0200

    protocole graphique : events new files/functions

---
+++ server_zappy/modules/graph_protocol/src/graph_event_egg.c
@@ -0,0 +1,39 @@
+/*
+** graph_event_egg.c for zappy project
+** 
+** Made by ramnes
+** <perso@ramnes.eu>
+** 
+** Started on  Fri Jul  8 02:37:42 2011 by ramnes
+** Last update Fri Jul  8 02:40:15 2011 ramnes
+*/
+
+#include "graph_events.h"
+
+/* EggNew */
+
+bool	graph_enw(void *data)
+{
+  return (true);
+}
+
+/* EggHatch */
+
+bool	graph_eht(void *data)
+{
+  return (true);
+}
+
+/* EggBound */
+
+bool	graph_ebo(void *data)
+{
+  return (true);
+}
+
+/* EggDied */
+
+bool	graph_edi(void *data)
+{
+  return (true);
+}
---
+++ server_zappy/modules/graph_protocol/src/graph_event_incant.c
@@ -0,0 +1,26 @@
+/*
+** graph_event_incant.c for zappy project
+** 
+** Made by ramnes
+** <perso@ramnes.eu>
+** 
+** Started on  Fri Jul  8 02:34:53 2011 by ramnes
+** Last update Fri Jul  8 02:37:09 2011 ramnes
+*/
+
+#include "graph_events.h"
+
+/* pic: IncaNew */
+
+bool	graph_pic(void *data)
+{
+  return (true);
+}
+
+/* pie: IncaEnd */
+
+bool	graph_pie(void *data)
+{
+  return (true);
+}
+
