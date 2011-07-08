f9858faac7f3bb386afff44758d9fe29fec2a851
Julien Di Marco <juliendimarco@me.com>
2011-07-08 03:21:14 +0200

    Map debug is enormous ...

--- server_zappy/src/run.c
+++ server_zappy/src/run.c
@@ -37,7 +37,7 @@ bool            run(void)
       exec_pool(gserv->pool, time_d(t));
       exec_event(time_d(t));
       exec_timer(&tv, time_d(t));
-#if	!defined(NDEBUG)
+#if	!defined(NDEBUG) && defined(MAPPRINT)
       dbg_show_map();
 #endif
     }
