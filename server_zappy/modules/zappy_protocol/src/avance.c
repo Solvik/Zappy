d6cc393291baef22d8468d6a03d9610861290a26
Julien Di Marco <juliendimarco@me.com>
2011-07-08 17:55:30 +0200

    Avance fixed Update the map - Subject

--- server_zappy/modules/zappy_protocol/src/avance.c
+++ server_zappy/modules/zappy_protocol/src/avance.c
@@ -29,11 +29,13 @@ int		zappy_avance(t_fds *c, char *_)
       return (false);
     }
   print_debug_(true, "[%d] - {x: %d, y: %d} -->", p->direction, p->x, p->y);
+  set_box_delplayer(p);
   p->x += (p->direction == WEST) ? 1 : (p->direction == EAST) ? -1 : 0;
   p->y += (p->direction == NORTH) ? 1 : (p->direction == SOUTH) ? -1 : 0;
   print_debug_(false, "[%d] - {x: %d, y: %d} -->", p->direction, p->x, p->y);
   p->x = X(p->x);
   p->y = Y(p->y);
+  set_box_addplayer(p, p->x, p->y);
   print_debug_(false, " {x: %d, y: %d}\n", p->x, p->y);
   sends(c, "ok");
   event_relative_dispatch("PlayerMove", c, 0);
