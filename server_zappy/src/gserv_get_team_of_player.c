c04ee69ef2ff63d2bfbf949fc9914e24085a9f18
Julien Di Marco <juliendimarco@me.com>
2011-07-10 01:51:06 +0200

    egg in tserv

--- server_zappy/src/gserv_get_team_of_player.c
+++ server_zappy/src/gserv_get_team_of_player.c
@@ -13,18 +13,7 @@
 
 extern t_server *gserv;
 
-static bool	match_player(void *data, void *arg)
-{
-  return (data == arg);
-}
-
-static bool	match_team(void *data, void *arg)
-{
-  return (get_data_as_arg(((t_team *)data)->players,
-			  match_player, arg));
-}
-
 t_team		*get_team_of_player(t_player *player)
 {
-  return (get_data_as_arg(gserv->team, match_team, player));
+  return (player ? player->team : NULL);
 }
