8f2b8f5bbe508083516ee745367e0a2e004fb430
guillaume gelin <perso@ramnes.eu>
2011-07-09 02:41:13 +0200

    solvik <3

--- server_zappy/modules/zappy_protocol/src/prend.c
+++ server_zappy/modules/zappy_protocol/src/prend.c
@@ -5,7 +5,7 @@
 ** Login   <blum_s@epitech.net>
 **
 ** Started on  Mon Jun 13 12:46:13 2011 solvik blum
-** Last update Mon Jul  4 01:23:02 2011 solvik blum
+** Last update Sat Jul  9 02:41:04 2011 ramnes
 */
 
 #include	<unistd.h>
@@ -40,14 +40,20 @@ static bool	prendre_action(int x, int y, char *obj)
 
 int		zappy_prend(t_fds *client, char *cmd)
 {
+  t_generic	*data;
   char		*obj;
 
+  if (!(data = malloc(sizeof(*data))))
+    return (false);
   strtok(cmd, " \t");
   obj = strtok(NULL, " \t");
   if (obj)
     {
       if (prendre_action(player_data->x, player_data->y, obj))
 	{
+	  data->ui1 = player_data->id;
+	  data->ui2 = 0; /* ramnes: TODO s/0/numéro de ressource/ */
+	  event_relative_dispatch("TakeItem", data, 0);
 	  sends(client, "ok");
 	  return (1);
 	}
