17a051ba530d66133494d3d032c19a6460cb10d9
Sebastien Blot <seb@seb-desktop.(none)>
2011-07-10 02:09:39 +0200

    segfault fix, nyan is back \o/

--- client_zappy/src/sound.c
+++ client_zappy/src/sound.c
@@ -13,19 +13,22 @@
 
 int		play_sound(t_fds *f, t_cmd *c, t_visu *v)
 {
-  FMOD_SYSTEM *system;
+  static FMOD_SYSTEM *system = NULL;
   FMOD_SOUND *musique;
 
   (void)f;
   (void)c;
   (void)v;
 
+  if (system)
+    {
+      FMOD_System_Close(system);
+      FMOD_System_Release(system);
+    }
   FMOD_System_Create(&system);
   FMOD_System_Init(system, 1, FMOD_INIT_NORMAL, NULL);
   FMOD_System_CreateSound(system, "nyan.wav", FMOD_SOFTWARE | FMOD_2D | FMOD_CREATESTREAM, 0, &musique);
   FMOD_Sound_SetLoopCount(musique, -1);
   FMOD_System_PlaySound(system, FMOD_CHANNEL_FREE, musique, 0, NULL);
-  FMOD_System_Close(system);
-  FMOD_System_Release(system);
   return (0);
 }
