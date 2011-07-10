84e769d87e72496d5690682490f7336e4fd74681
guillaume gelin <perso@ramnes.eu>
2011-07-10 04:41:26 +0200

    resolved conflict fork

--- server_zappy/modules/zappy_protocol/src/fork.c
+++ server_zappy/modules/zappy_protocol/src/fork.c
@@ -5,7 +5,7 @@
 ** Login   <blum_s@epitech.net>
 **
 ** Started on  Mon Jun 13 12:46:13 2011 solvik blum
-** Last update Sat Jul  9 18:52:08 2011 ramnes
+** Last update Sun Jul 10 04:40:23 2011 guillaume gelin
 */
 
 #include	<stdlib.h>
@@ -17,20 +17,20 @@ bool		egg_eclosion(void *data)
   t_player	*player;
 
   ((t_egg *)data)->team->max_conn++;
-  if (!(player = new_player(((t_egg *)data)->team->name)))
+  if ((player = new_player(((t_egg *)data)->team->name)) == NULL)
     return (false);
-  player->fork = true;
   return (true);
 }
 
-int		zappy_fork(t_fds *client, char *_)
+int		zappy_fork(t_fds *client, char *cmd)
 {
-  t_player	*p;
   t_egg		*egg;
 
   (void)_;
   if (!client || !(p = *(t_player**)client) || !(egg = set_box_addegg(p)))
     return (false);
+  event_relative_dispatch("Fork", client, 0);
+  /* ramnes: need sheduler 42/t */
   event_relative_dispatch("EggNew", egg, 0);
   event_relative_dispatch("EggHatch", egg, 600);
   sends(client, "ok");
