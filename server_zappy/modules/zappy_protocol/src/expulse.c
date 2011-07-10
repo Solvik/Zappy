489ba9ba9e00b31aa921d6824acda24807314534
Solvik <solvik.blum@gmail.com>
2011-07-10 02:23:15 +0200

    expulse fixed. thanks barbara for doing what i told ya

--- server_zappy/modules/zappy_protocol/src/expulse.c
+++ server_zappy/modules/zappy_protocol/src/expulse.c
@@ -24,19 +24,32 @@ static const char	*enum_to_dir[4] =
     "WEST",
   };
 
+static void	expulse_players(void *data, void *dest)
+{
+  t_fds		*client;
+
+  client = (t_fds *)data;
+  if (player_data != (t_player *)dest)
+    sends(data, "avance");
+}
+
 int		zappy_expulse(t_fds *client, char *cmd)
 {
   int		r;
   char		*ret;
   t_list	*list;
+  t_module      *mod;
 
   (void)cmd;
   list = get_box_players(player_data->x, player_data->y);
   if (get_list_len(list))
-    {
+    {      
+      mod = get_module_by_name("Zappy Protocol");
+      foreach_arg_list(mod->clients, expulse_players, player_data);
       r = asprintf(&ret, "deplacement %s",
 		   enum_to_dir[player_data->direction]);
       sends(client, ret);
+      expulse_players(list, player_data);
       if (ret)
 	free(ret);
       sends(client, "ok");
